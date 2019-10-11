4e09334ac8a195a0e3cc0103e0fd8b563bef4bac
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-11 02:03:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(phrase)
   if phrase == ""
    puts "NO, NOT SINCE 1938!"
-  elsif phrase =="I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
    puts "I LOVE YOU TOO PUMPKIN!"
   else
    puts "HUH?! SPEAK UP, SONNY!"
