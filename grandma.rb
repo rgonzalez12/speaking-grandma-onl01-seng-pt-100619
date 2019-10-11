4992d053a52ded1a5c2e67c69642e84724c4c2e5
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-11 02:13:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phase == phrase.upcase
+  elsif pharse == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "NO, NOT SINCE 1938!"
